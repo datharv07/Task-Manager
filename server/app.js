d4849add258c8f639c4bf4416d1b8fb98ca66178
datharv_07 <atharvdaware07@gmail.com>
2024-03-07 00:27:59 +0530

    comit 4

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://task-manager-5sbz.vercel.app');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
