3d01e1997cfe5294b82d014047c6400ba60693cb
datharv_07 <atharvdaware07@gmail.com>
2024-03-07 01:30:06 +0530

    comit

--- server/app.js
+++ server/app.js
@@ -11,21 +11,13 @@ import taskRouter from "./routes/taskRouter.js";
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-const corsOptions = {
-  origin: 'https://task-manager-5sbz.vercel.app',
-  credentials: true,
-};
-
-
-
-app.use(cors(corsOptions));
-
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://task-manager-5sbz.vercel.app');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
