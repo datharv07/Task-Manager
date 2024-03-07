c33582312255a5d2f20d8339d75a71db84ef402c
datharv_07 <atharvdaware07@gmail.com>
2024-03-07 12:17:54 +0530

    comit

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ app.get("/render", (req, res) => {
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', process.env.MONGO_URI);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
